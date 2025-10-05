743b0a3fd3866966d7a860f9afadbdbb0564b1fb
mohitagarwal24 <agarwalmohit545@gmail.com>
2025-10-05 17:52:33 +0000

    production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Optional but recommended settings
+      protocolImports: true, // allows imports like node:buffer
+      globals: {
+        Buffer: true,
+        process: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -11,5 +23,20 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+        'process.env': '{}',
+      },
+    },
+  },
+  define: {
+    global: 'window',
+    'process.env': {},
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
 });
